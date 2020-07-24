0d6e509285fec725b7a052fb015e68ab0a4e3614
Hunter Wilkins <hunter.wilkins2@gmail.com>
2020-07-23 22:00:25 -0400

    Change monad interface

--- src/main/java/com/hunter/monads/Monad.java
+++ src/main/java/com/hunter/monads/Monad.java
@@ -3,5 +3,5 @@ package src.main.java.com.hunter.monads;
 import java.util.function.Function;
 
 public interface Monad<A> extends Functor<A>, Applicative<A> {
-    public abstract <B> Monad<B> flatmap(Function<A, Monad<B>> f);
+    <B> Monad<B> flatmap(Function<A, Monad<B>> f);
 }
